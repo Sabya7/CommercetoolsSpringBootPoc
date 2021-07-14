9feb542bb889d561295f57609da54eade1c0828f
Sabya7 <51950160+Sabya7@users.noreply.github.com>
2021-07-14 10:49:31 +0530

    Handled error using Error classes in Extensions
    Signed-off-by: Sabya7 <sabya62.ss@gmail.com>

--- CommercetoolsSpringBootPoc/src/main/java/com/sabyacommercetools/poc/ExtensionsExploration.java
+++ CommercetoolsSpringBootPoc/src/main/java/com/sabyacommercetools/poc/ExtensionsExploration.java
@@ -1,10 +1,14 @@
 package com.sabyacommercetools.poc;
 
 import com.commercetools.api.client.ApiRoot;
-import com.commercetools.api.models.customer.Customer;
+import com.commercetools.api.models.customer.*;
+import com.commercetools.api.models.error.*;
 import com.commercetools.api.models.extension.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import io.vrap.rmf.base.client.ApiHttpResponse;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
@@ -14,6 +18,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.DataInput;
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -28,7 +34,7 @@ public class ExtensionsExploration {
 
         ExtensionDestination destination = ExtensionHttpDestinationBuilder.of()
 
-                .url("https://804e02bb0387ba.localhost.run/extension")
+                .url("https://45652bdfb87217.localhost.run/extension13")
                 .build();
 
         ExtensionTrigger trigger = ExtensionTriggerBuilder.of()
@@ -74,6 +80,7 @@ public class ExtensionsExploration {
 //        System.out.println("Customer : " + customer);
         JSONObject obj = new JSONObject();
         JSONArray actions = new JSONArray();
+        JSONArray errors = new JSONArray();
 
 
         JSONObject changeFirstNameAction = new JSONObject();
@@ -85,12 +92,115 @@ public class ExtensionsExploration {
             actions.appendElement(changeFirstNameAction);
         }
 
+        JSONObject error = new JSONObject();
+        error.put("code" , "RequiredField");
+        error.put("message"," Hello World");
+
+        errors.appendElement(error);
+
+
+        if(customerFName.contains("error"))
+        { obj.put("errors",errors);
+            return new ResponseEntity(obj, HttpStatus.BAD_REQUEST);
+        }
+        else
         obj.put("actions",actions);
+
 
 
         return new ResponseEntity(obj, HttpStatus.OK);
     }
 
+    @RequestMapping("/extension13")
+    public ResponseEntity extSab13(@RequestBody JsonNode req, @RequestHeader HttpHeaders headers) throws JsonProcessingException {
+        System.out.println("headers : " + headers);
+        System.out.println("reqBody : " + req);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JSR310Module());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+
+        Customer customer = objectMapper
+                .treeToValue(req.get("resource").get("obj"), Customer.class);
+        String customerName = customer.getFirstName();
+
+        CustomerSetFirstNameAction custAction =
+                CustomerSetFirstNameActionBuilder.of().firstName("Sabya").build();
+
+        CustomerUpdateAction customerUpdateAction = CustomerSetFirstNameAction.of()
+                .withCustomerSetFirstNameAction(a -> custAction);
+
+
+        CustomerUpdate customerUpdate = CustomerUpdateBuilder.of()
+                .actions(customerUpdateAction)
+                .build();
+        CustomerUpdate emptyCustomerUpdate = CustomerUpdateBuilder
+                .of()
+                .actions()
+                .build();
+
+        ErrorObject object = RequiredFieldErrorBuilder
+                .of()
+                .message("Sabya Custom Ex13 msg")
+                .build();
+
+        ErrorResponse errorResponse = ErrorResponseBuilder
+                .of()
+                .errors(object)
+                .build();
+
+        if(customerName.contains("abcd"))
+            return new ResponseEntity(errorResponse,HttpStatus.BAD_REQUEST);
+        else if(customerName.contains("137"))
+        return new ResponseEntity(customerUpdate, HttpStatus.CREATED);
+        else
+            return new ResponseEntity(emptyCustomerUpdate,HttpStatus.OK);
+    }
+    @GetMapping("/upExt")
+    public ApiHttpResponse<Extension> updateExtension() throws ExecutionException, InterruptedException {
+//        JSONObject object = new JSONObject();
+//        JSONArray array = new JSONArray();
+//
+//        JSONObject update = new JSONObject();
+//
+//        update.put("action", "changeDestination");
+//        update.put("destination","sabya.com");
+//
+//        array.appendElement(update);
+//        object.put("version",1);
+//        object.put("actions",object);
+        ExtensionDestination destination = ExtensionHttpDestinationBuilder.of()
+
+                .url("https://sabya.com")
+                .build();
+
+
+        ExtensionUpdateAction extensionUpdateAction = ExtensionChangeDestinationActionBuilder.of()
+                .destination(destination)
+                .build();
+
+        ExtensionUpdate extensionUpdate = ExtensionUpdateBuilder.of()
+                .actions(extensionUpdateAction)
+                .version(1L)
+                .build();
+
+        CompletableFuture<ApiHttpResponse<Extension>> response = client.withProjectKey("demo-project")
+                .extensions()
+                .withId("0f3cd171-61f3-466c-98ab-5e1f0f363436")
+                .post(extensionUpdate)
+                .execute();
+
+       return response.get();
+    }
+
+    @GetMapping("delExt")
+    public void delExt()
+    {
+        client.withProjectKey("demo-project")
+                .extensions()
+                .withId("")
+                .delete()
+                .execute();
+
+    }
 }
-@JsonSerialize
-class EmptyResp{}
